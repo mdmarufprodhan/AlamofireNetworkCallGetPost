88f18b24150c5f01cec14e441ac2b52b5c8879f1
mdmarufprodhan <marufprodhan463@gmail.com>
2022-09-11 00:58:13 +0600

    Initial Commit

---
+++ ApiCallGetAndPost/ApiCallGetAndPostApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiCallGetAndPostApp.swift
+//  ApiCallGetAndPost
+//
+//  Created by Md Maruf Prodhan on 11/9/22.
+//
+
+import SwiftUI
+
+@main
+struct ApiCallGetAndPostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
